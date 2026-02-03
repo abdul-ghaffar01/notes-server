7b2c53f9969c64d786c1b29faec6ae0d16e27bce
Abdul Ghaffar <agscontact777@gmail.com>
2026-02-03 22:46:24 +0500

    Create Note model

---
+++ internal/note/model.go
@@ -0,0 +1,10 @@
+package note
+
+import "time"
+
+type Note struct {
+	ID	  string `json:"id"`
+	Title string `json:"title"`
+	Description string `json: "description"`
+	Time time.Time `json: "time"`
+}
